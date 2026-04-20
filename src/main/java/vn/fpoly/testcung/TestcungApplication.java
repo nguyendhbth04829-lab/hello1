88c6565614cb3933a3fcb57569d0bed2ffd4f844
Nguyen <emailcuaban@gmail.com>
2026-04-20 07:54:29 +0700

    all

--- src/main/java/vn/fpoly/testcung/TestcungApplication.java
+++ src/main/java/vn/fpoly/testcung/TestcungApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestcungApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestcungApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TestcungApplication.class, args);
+    }
 
 }
--- src/test/java/vn/fpoly/testcung/methods/MyUtilTest.java
+++ src/test/java/vn/fpoly/testcung/methods/MyUtilTest.java
@@ -1,4 +1,17 @@
+package vn.fpoly.testcung.methods;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class MyUtilTest {
-  
+    MyUtil myUtil = new MyUtil();
+    @Test
+    void tinhtongn() {
+        assertEquals(6,myUtil.tinhtongtu1denn(3));
+    }
 }
